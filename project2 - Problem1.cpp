bab04b16460fb2901d7fac02b85d26dc1c84b664
ccastaneda85 <christopherj.castaneda@gmail.com>
2024-03-13 20:15:05 -0700

    project 1 problem 1
    initial

---
+++ project2 - Problem1.cpp
@@ -0,0 +1,37 @@
+/*
+Abstract:
+Develop a pseudocode for an algorithm; analyze your pseudocode mathematically; implement the code
+for the algorithm of your choice; test your implementation; and describe your results.
+
+Problem 1: Top k Most frequent elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the
+answer in any order.
+
+Example 1:
+Input: nums = [1, 1, 1, 2, 2, 3], k = 2
+Output: [1, 2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+
+Constraints:
+• 1 <= nums.length <= 105
+• -104 <= nums[i] <= 104
+• k is in the range [1, the number of unique elements in the array].
+• It is guaranteed that the answer is unique.
+
+Your code should work for any input array. Try to solve it using priority queue as it gives better time
+complexity in problems like this. But feel free to use any approach which is easy for you.
+
+*/
+
+/*
+Pseudocode:
+
+    int[] TopKMostFrequentElements(int a[], int k)
+
+
+
+*/
