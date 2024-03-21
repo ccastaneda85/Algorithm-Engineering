7b7edb3b4e06c1946451ed3b280a04792f5f679d
ccastaneda85 <christopherj.castaneda@gmail.com>
2024-03-21 12:28:43 -0700

    Project 2 Question 1 done

--- project2 - Problem1.cpp
+++ project2 - Problem1.cpp
@@ -30,8 +30,82 @@ complexity in problems like this. But feel free to use any approach which is eas
 /*
 Pseudocode:
 
-    int[] TopKMostFrequentElements(int a[], int k)
+    TopKMostFrequentElements(List, int k) -> returns list of top k elements
+        count the number of occurences in each element -> O(n)
+            store in a container of key, value pair // for example: [1, 3], [2,2], [3, 1] -> O(n)
+        sort the container in descending order of values -> O(nlogn)
+            return the keys of the first k elements and store into a new list -> O(k)
+            return the list to the caller. -> + 1
 
 
-
+    Total Time Complexity:
+        T(n)    = n + n + nlogn + k + 1
+                = 2n + nlogn + k + 1
+                = nlogn + k
+        T(n)    = O(nlogn + k)
 */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/* Created comparator function to assist in sort of the key value pairs */
+bool greater (std::pair<int,int> kv1, std::pair<int, int> kv2 ) {return (kv1.second > kv2.second);}
+
+/* Main function to accept Array, array size, and K for "K top elements"*/
+void topKMostFrequentElements(int List[], int size, int k) {
+    std::unordered_map<int, int> counter; // initializing unordered_map to assist in storing keys and counting the number of occurences of each key
+        for (int i = 0; i < size; i++) {
+            counter[List[i]] += 1; // once accessing a duplicated element the associated value will increment by 1, thus counting the number of each element's occurence
+        }
+
+    std::vector<std::pair<int, int>> sortedCounter(counter.begin(), counter.end()); // placing the key value pairs in a sortable container
+    std::sort(sortedCounter.begin(), sortedCounter.end(), greater); // sorting the container in descending order by the value of the key value pair
+
+
+    /* Begin output of Top K most frequent */
+    std::cout << "["; 
+
+    for (int i = 0; i < k; i++) {
+        if(i != k-1)
+            std::cout << sortedCounter[i].first << ", ";
+        else 
+            std::cout << sortedCounter[i].first;
+    }
+
+    std::cout << "]";
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int example1[] = {1, 1, 1, 2, 2, 3 };
+    int example2[] = {1};
+    int testArray2[] = {-1, -2, 4, 4, 4, -1, -1, -1};
+    
+    std::cout << std::endl;
+    std::cout << "Example 1: {1, 1, 1, 2, 2, 3 }";
+    std::cout << std::endl;
+    std::cout << "Top K elements: ";
+    topKMostFrequentElements(example1, 6, 2);
+    std::cout << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Example 2: {1}";
+    std::cout << std::endl;
+    std::cout << "Top K elements: ";
+    topKMostFrequentElements(example2, 1, 1);
+    std::cout << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Example 3: {-1, -2, 4, 4, 4, -1, -1, -1} ";
+    std::cout << std::endl;
+    std::cout << "Top K elements: ";
+    topKMostFrequentElements(testArray2, 8, 3);
+    std::cout << std::endl;
+    std::cout << std::endl;
+   
+
+    
+    return 0;
+}
